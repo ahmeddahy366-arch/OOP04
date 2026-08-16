56205cfbf0abb1dbae871a0b540eb6435a62e51f
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-08-16 19:57:10 +0300

    question2

--- ConsoleApp18-OOP04/Program.cs
+++ ConsoleApp18-OOP04/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp18_OOP04
+﻿using ConsoleApp18_OOP04;
+
+namespace ConsoleApp18_OOP04
 {
     internal class Program
     {
@@ -17,3 +19,16 @@
 
 
 #endregion
+#region question2
+//a)  What is the difference between an Abstract Class and an Interface?
+//Abstract Class: Represents an "is-a" relationship with shared code, fields, and constructors, but supports only single inheritance.
+//Interface: Represents a "can-do" capability defining a strict contract without fields or constructors, supporting multiple inheritance.
+//b)  When would you choose an Interface instead of an Abstract Class?
+//Multiple Inheritance: A class needs to adopt multiple behaviors,
+//as C# supports implementing multiple interfaces but inheriting from only one base class.
+//Defining Capabilities ("can-do")
+//No Shared State / Code: You only need to enforce a behavioral contract without sharing instance fields, state, or default code logic.
+//c)  Can a class inherit from multiple abstract classes? Can it implement multiple interfaces?
+//Multiple Abstract Classes: No.C# does not support multiple class inheritance.
+//Multiple Interfaces: Yes.A single class can implement multiple interfaces at the same time.
+    #endregion
